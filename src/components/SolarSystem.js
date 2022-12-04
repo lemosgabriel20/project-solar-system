d7f9a735490777847222ad36e281e1d4a1a629c1
Gabriel Lemos <jmjgb@outlook.com>
2022-12-04 06:29:55 -0300

    Renderizada lista de planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetas = planets.map((planet, index) => {
+      const { name } = planet;
+      const { image } = planet;
+      return <PlanetCard key={ index } planetName={ name } planetImage={ image } />;
+    });
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planetas }
       </div>
     );
   }
