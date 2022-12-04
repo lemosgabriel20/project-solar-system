75506adf423c9d953a8afbd47d2d557db2ddb5f2
Gabriel Lemos <jmjgb@outlook.com>
2022-12-04 07:12:48 -0300

    Componente Missions renderizado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missoes = missions.map((mission, index) => {
+      const { name } = mission;
+      const { year } = mission;
+      const { country } = mission;
+      const { destination } = mission;
+      return (
+        <MissionCard
+          key={ index }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      );
+    });
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missoes }
       </div>
     );
   }
